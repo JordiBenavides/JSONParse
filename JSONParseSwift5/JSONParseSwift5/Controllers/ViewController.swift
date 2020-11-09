9e19739acb74bc48eec8180583b15cc6c23a2f07
Jordi Benavides <jb.90@hotmail.es>
2020-11-09 17:40:08 +0100

    insert tableView delegate, datasource, register and model

--- JSONParseSwift5/JSONParseSwift5/Controllers/ViewController.swift
+++ JSONParseSwift5/JSONParseSwift5/Controllers/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableViewCell: UITableViewCell?
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "CustomTableViewCell")
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTableViewCell") as? CustomTableViewCell
+        return cell ??  UITableViewCell()
+    }
 
 }
 
---
+++ JSONParseSwift5/JSONParseSwift5/Model/Loan.swift
@@ -0,0 +1,18 @@
+//
+//  Loan.swift
+//  JSONParseSwift5
+//
+//  Created by Jordi Milla on 09/11/2020.
+//  Copyright © 2020 Jordi Milla Catalan. All rights reserved.
+//
+
+import Foundation
+
+struct Loan {
+ 
+    var name: String = ""
+    var country: String = ""
+    var use: String = ""
+    var amount: Int = 0
+ 
+}
